fa06dae32016806edf3f02bfe435ff0a5ab81d49
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-08-20 01:04:45 +0200

    chore: switch repo urls

--- src/plugin.config.ts
+++ src/plugin.config.ts
@@ -1,16 +1,16 @@
-import { createId } from "@paralleldrive/cuid2"
+import { createId } from "@paralleldrive/cuid2";
 
 export default {
-    ce_prefix: createId(),
-    identifier: 'com.snow.dislikes-skipper',
-    name: 'Dislikes Skipper',
-    description: 'Skip songs who have been disliked',
-    version: '1.0.0',
-    author: 'snow',
-    repo: 'https://github.com/kibbewater/dislikes-skipper',
-    entry: {
-        'plugin.js': {
-            type: 'main',
-        }
-    }
-}
+  ce_prefix: createId(),
+  identifier: "com.snow.dislikes-skipper",
+  name: "Dislikes Skipper",
+  description: "Skip songs who have been disliked",
+  version: "1.0.0",
+  author: "snow",
+  repo: "https://github.com/kibbewater/disliked-skipper",
+  entry: {
+    "plugin.js": {
+      type: "main",
+    },
+  },
+};
