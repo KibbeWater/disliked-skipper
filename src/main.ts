7ab278365b8442f88be679fab673dda57fee0704
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-08-25 19:49:50 +0200

    fix: improved startup reliability

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { useMusicKit } from './api/MusicKit.ts';
 import { PluginAPI } from './api/PluginAPI';
 import MySettings from './components/MySettings.vue';
 import config from './plugin.config.ts';
-import { customElementName } from './utils';
+import { customElementName, waitForMusicKit } from './utils';
 import { fetchRatings } from './utils/ratings.ts';
 import { useConfig } from './config.ts';
 
@@ -51,6 +51,7 @@ export default {
         const originalFunc = fetch;
         // @ts-ignore
         fetch = (url, opts) => {
+            const musicKit = useMusicKit();
             const urlPath = url.match(/https?:\/\/[^\/]+(\/[^?#]*)/)?.[1] || ''; // const urlPath = new URL(url).pathname;
             if (urlPath.startsWith('/v1/me/ratings/songs') && (opts.method === 'PUT' || opts.method === 'DELETE')) {
                 const songId = url.split('/').pop();
@@ -102,32 +103,35 @@ export default {
         };
 
         // Here we add a custom button to the top right of the chrome
-        const musicKit = useMusicKit();
-        musicKit.addEventListener('queueItemsDidChange', (queue: { id: string }[]) => {
-            const cfg = useConfig();
-            if (!cfg.enableSkipping || !cfg.enableCache) return; // This is a pre-caching check, dont run if caching is disabled
-
-            fetchRatings(queue.map((item) => item.id));
-        });
-        musicKit.addEventListener(
-            'queuePositionDidChange',
-            async ({ oldPosition, position }: { oldPosition: number; position: number }) => {
-                if (position === -1) return;
-
+        waitForMusicKit().then(() => {
+            console.log('[Dislikes Skipper] MusicKit is ready, adding event listeners');
+            const musicKit = useMusicKit();
+            musicKit.addEventListener('queueItemsDidChange', (queue: { id: string }[]) => {
                 const cfg = useConfig();
-                if (!cfg.enableSkipping) return;
+                if (!cfg.enableSkipping || !cfg.enableCache) return; // This is a pre-caching check, dont run if caching is disabled
 
-                const curSong = musicKit.queue.currentItem;
-                const ratings = await fetchRatings([curSong.id]);
+                fetchRatings(queue.map((item) => item.id));
+            });
+            musicKit.addEventListener(
+                'queuePositionDidChange',
+                async ({ oldPosition, position }: { oldPosition: number; position: number }) => {
+                    if (position === -1) return;
 
-                if (ratings[curSong.id]?.rating !== 'disliked') return;
-                const direction = position > oldPosition ? 'forward' : 'backward';
-                if (direction === 'forward') musicKit.skipToNextItem();
-                else musicKit.skipToPreviousItem();
-                console.log(
-                    `[Dislikes Skipper] Skipped ${direction} from disliked song "${curSong.attributes.artistName} - ${curSong.attributes.name.trim()}"`,
-                );
-            },
-        );
+                    const cfg = useConfig();
+                    if (!cfg.enableSkipping) return;
+
+                    const curSong = musicKit.queue.currentItem;
+                    const ratings = await fetchRatings([curSong.id]);
+
+                    if (ratings[curSong.id]?.rating !== 'disliked') return;
+                    const direction = position > oldPosition ? 'forward' : 'backward';
+                    if (direction === 'forward') musicKit.skipToNextItem();
+                    else musicKit.skipToPreviousItem();
+                    console.log(
+                        `[Dislikes Skipper] Skipped ${direction} from disliked song "${curSong.attributes.artistName} - ${curSong.attributes.name.trim()}"`,
+                    );
+                },
+            );
+        });
     },
 } as PluginAPI;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,24 @@
 import { useCPlugin } from '../api/CPlugin';
+import { useMusicKit } from '../api/MusicKit';
 import { CustomElements } from './../main';
 
 /**
  * Use this when you want to reference a custom element for this plugin.
  * @param name The name of the custom element.
- * @returns 
+ * @returns
  */
 export function customElementName(name: keyof typeof CustomElements) {
-    return `${useCPlugin().ce_prefix}-${name}`
+    return `${useCPlugin().ce_prefix}-${name}`;
+}
+
+export function waitForMusicKit(): Promise<void> {
+    return new Promise((resolve) => {
+        const interval = setInterval(() => {
+            const musicKit = useMusicKit();
+            if (musicKit?.addEventListener) {
+                clearInterval(interval);
+                resolve();
+            }
+        }, 100);
+    });
 }
